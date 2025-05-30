a80b8c1c51ac163f488d1adf8f94c72427ec7e42
revoladesign <revola204@gmail.com>
2025-05-30 06:00:42 +0100

    Update script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.gallery img').forEach(img => {
-  img.addEventListener('click', () => {
-    document.getElementById("signup-popup").style.display = "flex";
-  });
-});
-
-document.querySelector('.close').addEventListener('click', () => {
-  document.getElementById("signup-popup").style.display = "none";
-});
