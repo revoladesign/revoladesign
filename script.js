3a0d5a0993459174241c5bb6b84057128cf95788
revoladesign <revola204@gmail.com>
2025-05-30 17:56:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 <script>
-  function openSidebar() {
-    document.getElementById("sidebar").classList.add("active");
+  function openMenu() {
+    document.getElementById("menuOverlay").classList.add("active");
   }
 
-  function closeSidebar() {
-    document.getElementById("sidebar").classList.remove("active");
+  function closeMenu() {
+    document.getElementById("menuOverlay").classList.remove("active");
   }
 
-  // ربط الضغط على الأيقونة بفتح القائمة
-  document.querySelector('.menu-icon').addEventListener('click', openSidebar);
+  document.querySelector('.menu-icon').addEventListener('click', openMenu);
 </script>
